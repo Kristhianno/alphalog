fc7878c9ccfb18785a644a95210c84d4deec6e67
Efraim Kristhianno <efraim@MacBook-Air-de-Efraim.local>
2026-08-10 13:00:29 -0300

    atualizacao

--- src/features/auth/BrandPanel.tsx
+++ src/features/auth/BrandPanel.tsx
@@ -2,8 +2,8 @@ import logoHorizontal from "@/assets/logo-horizontal.png"
 
 export function BrandPanel() {
   return (
-    <div className="hidden items-center justify-center border-r-4 border-primary bg-sidebar lg:flex">
-      <img src={logoHorizontal} alt="AlphaLog" className="h-14 w-auto" />
+    <div className="hidden items-center justify-center bg-sidebar lg:flex">
+      <img src={logoHorizontal} alt="AlphaLog" className="h-20 w-auto" />
     </div>
   )
 }
